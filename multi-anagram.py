f8b9a89466a56ba65966b06e01f1034043a72b43
Reyna P <ReynaP_03@hotmail.com>
2025-05-22 06:53:53 -0400

    Update and rename quad-anagram.py to multi-anagram.py

--- multi-anagram.py
+++ multi-anagram.py
@@ -1,17 +1,18 @@
 import twl
 
+word_length = int(input("Number of Letters: "))
 for input_word in set(twl.iterator()):
-    if len(input_word) == 4:
+    if len(input_word) == word_length:
         temp_list = list(twl.anagram(input_word))
         fin_list = []
         for temp_word in temp_list:
-            if len(temp_word) == 4:
+            if len(temp_word) == word_length:
                 fin_list.append(temp_word)
-        counter=0
+        counter = 0
         letter_list = list(input_word)
         for fin_word in fin_list:
             if fin_word[0] in letter_list:
                 letter_list.remove(fin_word[0])
                 counter += 1
-        if counter == 4:
+        if counter == word_length:
             print(fin_list)
